6750f4c4be592e93823c43d9ebed03472f69d09b
MikeTR322 <michaeltran32@gmail.com>
2024-05-09 07:45:36 -0500

    Polish Scrip
    Added function for drag and drop with additional snapping to position

--- Assets/DragAndDrop/Script/DragAndDrop.cs
+++ Assets/DragAndDrop/Script/DragAndDrop.cs
@@ -1,24 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
     Vector3 _mousePosition;
+
+    public bool _canSnapToPosition = false;
+    public List<Transform> _snapPoints;
+    public float _snapRange = 0.5f;
     public Camera _POVCamera = null;
 
+    private void Update()
+    {
+        if (_canSnapToPosition)
+        {
+            SnapToPosition();
+        }
+    }
+
+    //Get mouse position on Screen in Camera View
     private Vector3 GetMousePos()
     {
         return _POVCamera.WorldToScreenPoint(transform.position);
     }
 
+    //When mouse clicks on an object with the DragAndDrop script
     private void OnMouseDown()
     {
         _mousePosition = Input.mousePosition - GetMousePos();
     }
 
+    //The object follows the mouse position
     private void OnMouseDrag()
     {
+
         transform.position = _POVCamera.ScreenToWorldPoint(Input.mousePosition - _mousePosition);
+    }
+
+    private void SnapToPosition()
+    {
+        foreach(Transform point in _snapPoints)
+        {
+            if(Vector2.Distance(point.position,transform.position) <= _snapRange)
+            {
+                transform.position = point.position;
+                return;
+            }
+        }
     }
 }
